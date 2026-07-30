b54e6cae4b888ca68c7f3f79ea2c975b7e54d1d3
eratolabcool <eratolabcool@gmail.com>
2026-07-31 01:08:35 +0800

    Add Finaily research data loader

---
+++ finaily/apps/web/lib/research.ts
@@ -0,0 +1,21 @@
+import companies from "../../../database/companies.json";
+import themes from "../../../database/themes.json";
+import reports from "../../../database/reports.json";
+
+export function getCompanies() {
+  return companies;
+}
+
+export function getCompany(symbol: string) {
+  return companies.find(
+    (company) => company.symbol.toLowerCase() === symbol.toLowerCase()
+  );
+}
+
+export function getThemes() {
+  return themes;
+}
+
+export function getReports() {
+  return reports;
+}
